3c1e9e2134b6246b7bd357e3856cad1fcaa7a07c
Enis <hiherz67@gmail.com>
2022-04-21 06:31:13 +0200

    Ajout du formulaire (non fini)

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +15,24 @@ class BaseController extends AbstractController
     #[Route('/', name: 'app_base')]
     public function index(): Response
     {
-        return $this->render('base/index.html.twig', [
-            'controller_name' => 'BaseController',
+        // just set up a fresh $task object (remove the example data)
+        $task = new User();
+
+        $form = $this->createForm(RegisterType::class, $task);
+
+//        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->renderForm('base/end.html.twig');
+        }
+
+        return $this->renderForm('base/index.html.twig', [
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $mail;
+
+    #[ORM\Column(type: 'integer')]
+    private $telephoneNumber;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $code;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $codeActivated;
+
+    #[ORM\Column(type: 'datetime')]
+    private $creationTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getTelephoneNumber(): ?int
+    {
+        return $this->telephoneNumber;
+    }
+
+    public function setTelephoneNumber(int $telephoneNumber): self
+    {
+        $this->telephoneNumber = $telephoneNumber;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCodeActivated(): ?string
+    {
+        return $this->codeActivated;
+    }
+
+    public function setCodeActivated(string $codeActivated): self
+    {
+        $this->codeActivated = $codeActivated;
+
+        return $this;
+    }
+
+    public function getCreationTime(): ?\DateTimeInterface
+    {
+        return $this->creationTime;
+    }
+
+    public function setCreationTime(\DateTimeInterface $creationTime): self
+    {
+        $this->creationTime = $creationTime;
+
+        return $this;
+    }
+}
